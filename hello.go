b9bf0f066191514c620dd16376d25eb69ff061f9
samkeen <samkeen@lululemon.com>
2018-05-21 20:45:58 -0700

    cleanup

--- hello.go
+++ hello.go
@@ -1,20 +1,5 @@
 package main
 
-/*
-bin/
-    hello                 # command executable
-pkg/
-    linux_amd64/          # this will reflect your OS and architecture
-        github.com/user/
-            stringutil.a  # package object
-src/
-    github.com/user/
-        hello/
-            hello.go      # command source
-        stringutil/
-            reverse.go    # package source
- */
-
 import (
 	"fmt"
 	"github.com/samkeen/go-stringUtils"
